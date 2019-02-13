7bd6d4edc746ffc176b246d324baee73537053f1
MrYANG23 <1594737268@qq.com>
2019-02-13 16:07:36 +0800

    指针递减

--- main.cpp
+++ main.cpp
@@ -3,24 +3,43 @@ using namespace std;
 
 const int MAX=3;
 
-int main(){
-    int var[MAX]={10,11,12};
+//int main(){
+//    int var[MAX]={10,11,12};
+//    int *ptr;
+//    ptr=var;
+//    for (int i=0; i<MAX;i++)
+//    {
+//        cout<<"address of var["<<i<<"]=";
+//        cout<<ptr<<endl;
+//
+//        cout<<"value of var["<<i<<"]=";
+//        cout<<*ptr<<endl;
+//
+//        ptr++;
+//
+//    }
+//    return 0;
+//}
+
+int main()
+{
+    int var[MAX]={13,12,11};
     int *ptr;
-    ptr=var;
-    for (int i=0; i<MAX;i++)
+
+    ptr=&var[MAX-1];
+    for (int i=MAX;i>0;i--)
     {
-        cout<<"address of var["<<i<<"]=";
+        cout<<"adress of var["<<i<<"]=";
         cout<<ptr<<endl;
 
         cout<<"value of var["<<i<<"]=";
         cout<<*ptr<<endl;
 
-        ptr++;
+        ptr--;
+
 
     }
-    return 0;
 
 
 }
-
 
